1d98a30e3a65b60287984c1dab23b47e3c55466b
Jan Heil <office@byte-store.de>
2024-06-03 19:30:20 +0200

    Extend Hostinger class and refactor Cart features
    Expanded functionality within the Hostinger class by adding methods for retrieving credentials, prefixes, API channel, and debug console messaging. In addition, a significant enhancement was made to the Cart class under e-commerce, introducing new methods for product handling, local storage interactions, and server synchronization.

--- assets/ts/Hostinger.ts
+++ assets/ts/Hostinger.ts
@@ -1,15 +1,25 @@
 import {ECommerce} from "./ecommerce/ECommerce";
 import {Hosting} from "./hosting/Hosting";
 
-class Hostinger {
-    // Store Debug State.
-    public static debugIO : boolean = false;
-
+/**
+ * Represents the Hostinger SDK class.
+ */
+export class Hostinger {
     // Store ECommerce Instance.
     private ecommerceIO: ECommerce = new ECommerce();
 
     // Store Hosting Instance.
     private hostingIO: Hosting = new Hosting();
+
+    /**
+     * Object representing the credentials for I/O operations.
+     *
+     * @typedef {Object} CredentialsIO
+     * @property {string} url - The URL for the I/O operations.
+     */
+    public static credentialsIO = {
+        url: "127.0.0.1"
+    }
 
     /**
      * Retrieves the ecommerceIO object.
@@ -27,6 +37,43 @@ class Hostinger {
      */
     public getHosting() : Hosting {
         return this.getHosting();
+    }
+
+    /**
+     * Returns the prefix used by Hostinger.
+     *
+     * @return {string} The prefix used by Hostinger.
+     */
+    static getPrefix() {
+        return "hostinger";
+    }
+
+    /**
+     * Retrieves the Hostinger credentials.
+     *
+     * @returns {Object} - The Hostinger credentials object.
+     */
+    static getCredentials() {
+        return Hostinger.credentialsIO;
+    }
+
+    /**
+     * Fetches the API channel based on the given routeIO.
+     *
+     * @param {string} routeIO - The routeIO for which the API channel is to be fetched.
+     * @return {string} - The API channel corresponding to the given routeIO.
+     */
+    static getAPIChannel(routeIO: string) {
+        return this.getCredentials().url  + "/internal/" + routeIO;
+    }
+
+    /**
+     * Write a debug message to console.
+     * @param {string} messageIO - The message to be displayed in console.
+     * @return {undefined}
+     */
+    static debug(messageIO : string) {
+        console.debug(messageIO);
     }
 }
 
--- assets/ts/ecommerce/Cart.ts
+++ assets/ts/ecommerce/Cart.ts
@@ -1,7 +1,157 @@
+import {Hostinger} from "../Hostinger";
+
 /**
  * Represents ECommerce Cart Features of Hostinger.
  * @class
  */
 export class Cart {
+    // Store API Endpoint of Cart.
+    private static urlIO = "/internal/ecommerce/cart";
 
+    // If true Cart will be stored on Local Browser Storage.
+    private allowLocalIO: boolean;
+
+    // Store Products in Cart.
+    private itemsIO : Array<any> = new Array();
+
+    /**
+     * Set the state of allowing local IO.
+     *
+     * @param {boolean} stateIO - The new state of allowing local IO.
+     *
+     * @return {void}
+     */
+    public allowStoreLocal(stateIO : boolean) {
+        this.allowLocalIO = stateIO;
+
+        // Remove old Data from Browser Storage.
+        if(!this.allowLocalIO){
+            this.removeOldStorage();
+        }
+    }
+
+    /**
+     * Adds a product to the itemsIO array.
+     *
+     * @param {Object} itemIO - The product to be added.
+     */
+    public addProduct(itemIO: object) {
+        this.itemsIO.push(itemIO);
+
+        // Save to Server (optional Browser).
+        this.handleSave();
+    }
+
+    /**
+     * Removes the specified product from the itemsIO array.
+     *
+     * @param {object} itemIO - The object representing the product to be removed.
+     * @return {undefined}
+     */
+    public removeProduct(itemIO : object) {
+        delete this.itemsIO[this.itemsIO.indexOf(itemIO)];
+
+        // Save to Server (optional Browser).
+        this.handleSave();
+    }
+
+
+    /**
+     * Retrieves the list of products.
+     *
+     * @param {boolean} syncIO - Specifies whether to sync data from the server before retrieving the products.
+     * @return {Array} The list of products.
+     */
+    public getProducts(syncIO : boolean) {
+        if(syncIO)
+            this.syncFromServer();
+
+        return this.itemsIO;
+    }
+
+    /**
+     * Retrieves the cart data from the local storage.
+     *
+     * @private
+     * @return {Array<any>} The cart data as an array or null if not found.
+     */
+    private getLocalStorage() : Array<any> {
+        return JSON.parse(localStorage.getItem(Hostinger.getPrefix()  + ".cart"));
+    }
+
+    /**
+     * Saves the current cart items to the browser's local storage.
+     *
+     * @private
+     * @return {void}
+     */
+    protected saveToBrowser() {
+        localStorage.setItem(Hostinger.getPrefix() + ".cart", JSON.stringify(this.itemsIO));
+    }
+
+    /**
+     * Synchronizes data from the server.
+     *
+     * @private
+     * @method syncFromServer
+     * @returns {void}
+     * @todo Sync.
+     * @todo Parse Data
+     */
+    private syncFromServer() {
+        // @todo: Sync.
+        $.ajax(Hostinger.getAPIChannel(Cart.urlIO), {
+            method: "GET",
+            dataType: "json",
+            success: (dataIO => {
+                // @todo Parse Data
+            })
+        })
+    }
+
+    /**
+     * Sends the items to the server and saves them.
+     *
+     * @private
+     * @returns {void}
+     */
+    private saveToServer() {
+        $.ajax(Hostinger.getAPIChannel(Cart.urlIO), {
+            method: "POST",
+            dataType: "json",
+            data: JSON.stringify(this.itemsIO),
+            success: (dataIO => {
+                // @todo Parse Data
+            })
+        })
+    }
+
+    /**
+     * Handles the add operation by saving the data.
+     * @returns {void}
+     */
+    protected handleSave() {
+        // Only Save to Browser if allowed (Cookie Consent etc).
+        if(this.allowLocalIO)
+            this.saveToBrowser()
+
+        // Save to Server.
+        this.saveToServer();
+
+        // Print Debug Message.
+        Hostinger.debug("Saved Cart Array to Server"+ (this.allowLocalIO ? " and to Browser Storage" : "") +".");
+    }
+
+
+    /**
+     * Removes the old cart entry from local storage.
+     *
+     * @return {void} - This method does not return a value.
+     */
+    private removeOldStorage() {
+        localStorage.removeItem(Hostinger.getPrefix() + ".cart");
+
+        // Print Debug Message.
+        Hostinger.debug("Removed old Cart Entry from Local Storage.");
+    }
 }
